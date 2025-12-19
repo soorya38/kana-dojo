cceb193815baa1c62dcbb8e6a4fe0ae0b09d94f1
Gentrit <gentritzheg@gmail.com>
2025-12-19 17:49:02 +0100

    feat(types): define Organization type

--- features/Legal/credits/types.ts
+++ features/Legal/credits/types.ts
@@ -1,8 +1,13 @@
 export type Person = {
-  login: string
-  avatar: string
-  url?: string
-}
+  login: string;
+  avatar: string;
+  url?: string;
+};
 
-export type Contributor = Person
-export type Sponsor = Person
+export type Contributor = Person;
+export type Sponsor = Person;
+
+export type Organization = {
+  owner: Person;
+  members: Person[];
+};
